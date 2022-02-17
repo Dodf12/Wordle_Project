96188d61b11deb57d94fa3a9872e59842d0188ab
nnim583 <99527322+nnim583@users.noreply.github.com>
2022-02-17 12:06:34 -0800

    Update greetings.py

--- greetings.py
+++ greetings.py
@@ -1,8 +1,7 @@
 def Intro():
     print("Let's play the game of Wordle!!")
-    name = input("Player 1, Enter your name")
-    print("Welcome", name +"!")
-    a = ("Ok " + name," Are You Ready? (Enter Yes or No)")
+    name = input("Player 1, Enter your name: ")
+    a = "Welcome " + name + ", Are You Ready To Play The Game Of Wordle? (Enter Yes or No): "
     ready = input(a)
     if ready == "Yes":
         print("Loading You In To The Game . . .")
@@ -11,8 +10,10 @@ def Intro():
     else:    
         new_attempt = ""
         while (new_attempt != "Yes" and new_attempt != "No"):        
-            new_attempt = input("That is not a valid input, please try again!")
+            new_attempt = input("That is not a valid input, please try again!: ")
         if new_attempt == "Yes":
             print("Loading You In To The Game . . .")
         elif new_attempt == "No":
             print("Exiting You Out Of The Game . . .")
+b = Intro()
+print(b)
