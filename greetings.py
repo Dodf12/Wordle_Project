5f6fe299e52cf770b825aec752784d9ae1101a2f
nnim583 <99527322+nnim583@users.noreply.github.com>
2022-02-17 12:26:27 -0800

    Update greetings.py

--- greetings.py
+++ greetings.py
@@ -2,12 +2,15 @@ def Intro():
     print("Let's play the game of Wordle!!")
     name = input("Player 1, Enter your name: ")
     a = "Welcome " + name + ", Are You Ready To Play The Game Of Wordle? (Enter Yes or No): "
+    b = "If You Wanted To Say Maybe, Then Why Are You Playing The Game In The First Place 🙄 "
     ready = input(a)
+    mayb = input(b)
     if ready == "Yes":
         print("Loading You In To The Game . . .")
     elif ready == "No":
         print("Exiting You Out Of The Game . . .")
-    else:    
+    elif mayb == "Maybe":
+        input(b)
         new_attempt = ""
         while (new_attempt != "Yes" and new_attempt != "No"):        
             new_attempt = input("That is not a valid input, please try again!: ")
@@ -15,5 +18,20 @@ def Intro():
             print("Loading You In To The Game . . .")
         elif new_attempt == "No":
             print("Exiting You Out Of The Game . . .")
+        elif new_attempt == "Maybe":
+            print("Done Taking Maybes From You, Im Kicking You Out Of The Game Instead 🙄 ")
+    else:    
+        new_attempt = ""
+        while (new_attempt != "Yes" and new_attempt != "No" and new_attempt != "Maybe"):        
+            new_attempt = input("That is not a valid input, please try again!: ")
+        if new_attempt == "Yes":
+            print("Loading You In To The Game . . .")
+        elif new_attempt == "No":
+            print("Exiting You Out Of The Game . . .")
+        elif new_attempt == "Maybe":
+            print("Done Taking Maybes From You, Im Kicking You Out Of The Game Instead 🙄 ")
 b = Intro()
 print(b)
+
+
+                  
