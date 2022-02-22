9e12a4216d014d615e99e48cc7a2520aa95fd78b
dodf12 <geokrazi@gmail.com>
2022-02-21 18:06:23 -0800

    created files with proper uppercase

---
+++ Greetings.py
@@ -0,0 +1,37 @@
+def Intro():
+    print("Let's play the game of Wordle!!")
+    name = input("Player 1, Enter your name: ")
+    a = "Welcome " + name + ", Are You Ready To Play The Game Of Wordle? (Enter Yes or No): "
+    b = "If You Wanted To Say Maybe, Then Why Are You Playing The Game In The First Place 🙄 "
+    ready = input(a)
+    mayb = input(b)
+    if ready == "Yes":
+        print("Loading You In To The Game . . .")
+    elif ready == "No":
+        print("Exiting You Out Of The Game . . .")
+    elif mayb == "Maybe":
+        input(b)
+        new_attempt = ""
+        while (new_attempt != "Yes" and new_attempt != "No"):        
+            new_attempt = input("That is not a valid input, please try again!: ")
+        if new_attempt == "Yes":
+            print("Loading You In To The Game . . .")
+        elif new_attempt == "No":
+            print("Exiting You Out Of The Game . . .")
+        elif new_attempt == "Maybe":
+            print("Done Taking Maybes From You, Im Kicking You Out Of The Game Instead 🙄 ")
+    else:    
+        new_attempt = ""
+        while (new_attempt != "Yes" and new_attempt != "No" and new_attempt != "Maybe"):        
+            new_attempt = input("That is not a valid input, please try again!: ")
+        if new_attempt == "Yes":
+            print("Loading You In To The Game . . .")
+        elif new_attempt == "No":
+            print("Exiting You Out Of The Game . . .")
+        elif new_attempt == "Maybe":
+            print("Done Taking Maybes From You, Im Kicking You Out Of The Game Instead 🙄 ")
+c = Intro()
+print(c)
+
+
+                  
