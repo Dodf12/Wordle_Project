ac0fedb4a26e69ad281f553ce74e007fb15facca
Abhinav <87663453+Dodf12@users.noreply.github.com>
2022-03-10 16:21:14 -0800

    finally

--- WordlePlayer.py
+++ WordlePlayer.py
@@ -113,7 +113,7 @@ class WordlePlayer(Player):
             elif (x[i]==3): 
                 print (i+1, ": ############# ", x[i])     
             elif (x[i]==4): 
-                print (i+), ": ################# ", x[i])   
+                print (i+1, ": ################# ", x[i])   
             elif (x[i]==5): 
                 print (i+1, ": ##################### ", x[i])                           
             i=i+1
